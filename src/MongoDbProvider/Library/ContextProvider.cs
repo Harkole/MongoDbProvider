c38308a5eea37a483cffb77aea4f9ae9fb98b2c6
will.blackburn <will.blackburn@cubic-interactive.com>
2019-10-03 22:20:05 +0100

    Implemented Provider Library

---
+++ src/MongoDbProvider/Library/ContextProvider.cs
@@ -0,0 +1,20 @@
+using MongoDB.Driver;
+using MongoDbProvider.Interfaces;
+using MongoDbProvider.Modules;
+
+namespace MongoDbProvider.Library
+{
+    public class ContextProvider<T>
+    {
+        public IMongoCollection<T> Context { get; set; }
+
+        public ContextProvider(IDatabaseOptions options)
+        {
+            // Create the (Singleton) Client, Database and then return the Context ready for use
+            Context = MongoProvider
+                .CreateMongoClient(options.ConnectionString)
+                .GetDatabase(options.DatabaseName)
+                .GetCollection<T>(Pluralizer.Pluralize(nameof(T)));
+        }
+    }
+}
