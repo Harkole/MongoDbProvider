cbb355a02ce5fb82029a88cd97de452ea3c876ef
will.blackburn <will.blackburn@cubic-interactive.com>
2019-10-03 20:27:52 +0000

    Base Setup

---
+++ src/MongoDbProvider/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MongoDbProvider
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MonogDbProvider.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MonogDbProvider.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
