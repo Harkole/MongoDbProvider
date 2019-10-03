7e33a8af8557a2cc39d314a68041d546c114aedc
will.blackburn <will.blackburn@cubic-interactive.com>
2019-10-03 22:26:09 +0100

    Improved/Added comments

--- src/MongoDbProvider/ContextProvider.cs
+++ src/MongoDbProvider/ContextProvider.cs
@@ -6,8 +6,17 @@ namespace MongoDbProvider
 {
     public class ContextProvider<T>
     {
+        /// <summary>
+        /// Provides the accessor to the Context<T>, where <T> is the data 
+        /// model/schema to use in the collection. The collection name will
+        /// attempt to pluralize the <T> name
+        /// </summary>
         public IMongoCollection<T> Context { get; set; }
 
+        /// <summary>
+        /// Constructs the Context with the Database Options values provided
+        /// </summary>
+        /// <param name="options">The connection string and name of the Database to use</param>
         public ContextProvider(IDatabaseOptions options)
         {
             // Create the (Singleton) Client, Database and then return the Context ready for use
--- src/MongoDbProvider/Interfaces/IDatabaseOptions.cs
+++ src/MongoDbProvider/Interfaces/IDatabaseOptions.cs
@@ -2,7 +2,14 @@ namespace MongoDbProvider.Interfaces
 {
     public interface IDatabaseOptions
     {
+        /// <summary>
+        /// Connection string to the MongoDB server
+        /// </summary>
         string ConnectionString { get; set; }
+
+        /// <summary>
+        /// The Database to use within the MongoDb Server
+        /// </summary>
         string DatabaseName { get; set; }
     }
 }
